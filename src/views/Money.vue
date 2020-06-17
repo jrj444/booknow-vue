5326325ca9402c1388ed92655f37ce8cc0a48bd1
jiango <jrj444@foxmail.com>
2020-06-17 22:46:27 +0800

    完成部分Money页面样式

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -14,7 +14,7 @@
     <div>
       <label class="remark">
         <span class="name">备注</span>
-        <input type="text">
+        <input type="text" placeholder="在这里输入备注">
       </label>
     </div>
     <div>
@@ -51,5 +51,81 @@
   };
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+  @import "~@/assets/style/helper.scss";
+
+  .types {
+    background: #c4c4c4;
+    display: flex;
+    text-align: center;
+    font-size: 24px;
+
+    > li {
+      width: 50%;
+      height: 64px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      position: relative;
+
+      &.selected::after {
+        content: '';
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        width: 100%;
+        height: 4px;
+        background: #333333;
+      }
+    }
+  }
+
+  .remark {
+    display: flex;
+    font-size: 14px;
+    background: #e5e5e5;
+    padding: 16px;
+    line-height: 22px;
+
+    .name {
+      margin-right: 16px;
+    }
+
+    input {
+      border: none;
+      background: transparent;
+      flex-grow: 1;
+    }
+  }
+
+  .tags {
+    padding: 16px;
+    font-size: 14px;
+
+    > .exist-tags {
+      display: flex;
+
+      > li {
+        display: flex;
+        align-items: center;
+        background: #d9d9d9;
+        height: 24px;
+        border-radius: 12px;
+        padding: 0 18px;
+        margin-right: 16px;
+      }
+    }
+
+    > .new-tag {
+      padding-top: 16px;
+
+      button {
+        background: transparent;
+        border: none;
+        color: #999999;
+        border-bottom: 1px solid;
+        padding: 0 4px;
+      }
+    }
+  }
 </style>
