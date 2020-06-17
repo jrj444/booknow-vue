26ca0a1a52464a99343d6f39c104f2aeb9f92fbd
jiango <jrj444@foxmail.com>
2020-06-17 11:23:13 +0800

    添加Money页面布局

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
-    Money
+  <div class="Nav-wrapper">
+    <div class="Money-content">
+      Money
+    </div>
     <Nav />
   </div>
 
@@ -13,5 +15,14 @@
 </script>
 
 <style scoped>
-
+  .Nav-wrapper {
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+  }
+  .Money-content {
+    border: 1px solid blue;
+    flex-grow: 1;
+    overflow: auto;
+  }
 </style>
