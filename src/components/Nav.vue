66222eed5d93c06d4bba2ab7a10c17801324434b
jiango <jrj444@foxmail.com>
2020-06-17 16:12:51 +0800

    添加导航栏选中效果

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,16 +1,16 @@
 <template>
   <nav>
-    <router-link to="/money" class="item">
+    <router-link to="/money" class="item" active-class="selected">
       <Icon name="money"/>
       记账
     </router-link>
 
-    <router-link to="/labels" class="item">
+    <router-link to="/labels" class="item" active-class="selected">
       <Icon name="labels"/>
       标签
     </router-link>
 
-    <router-link to="/statistics" class="item">
+    <router-link to="/statistics" class="item" active-class="selected">
       <Icon name="statistics"/>
       统计
     </router-link>
@@ -30,9 +30,8 @@
   nav {
     display: flex;
     flex-direction: row;
-    box-shadow: 0 0 3px rgba(0,0,0,0.25);
+    box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
     font-size: 12px;
-
 
     > .item {
       padding: 3px 0;
@@ -41,10 +40,14 @@
       justify-content: center;
       align-items: center;
       flex-direction: column;
+
       .icon {
         width: 32px;
         height: 32px;
       }
+    }
+    > .item.selected {
+      color: orangered;
     }
   }
 </style>
