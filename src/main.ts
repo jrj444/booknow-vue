f430205789ccf1200b140512f79a83edbd8cc78a
jiango <jrj444@foxmail.com>
2020-06-22 22:20:01 +0800

    封装windoow.createTagå全局方法创建标签

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,14 @@ Vue.component('Nav', Nav);
 Vue.component('Layout', Layout);
 
 window.tagList = tagListModel.fetch();
+window.createTag = (name) => {
+  const message = tagListModel.create(name);
+  if (message === 'duplicated') {
+    window.alert('标签名重复啦，请重新创建！');
+  } else if (message === 'success') {
+    window.alert('添加成功！');
+  }
+};
 
 new Vue({
   router,
