930483256c8b187dc1a684eff2d9eefb0cb44313
jiango <jrj444@foxmail.com>
2020-06-22 10:08:48 +0800

    添加标签页面路由跳转至编辑标签页面

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,11 +1,11 @@
 <template>
   <Layout>
-    <ul class="tags">
-      <li v-for="tag in tags" :key="tag.id">
+    <div class="tags">
+      <router-link class="tag" v-for="tag in tags" :key="tag.id" :to="`/labels/edit/${tag.id}`">
         <span>{{tag.name}}</span>
         <Icon name="right"/>
-      </li>
-    </ul>
+      </router-link>
+    </div>
     <div class="create-tag-wrapper">
       <button class="create-tag" @click="createTag">新建标签</button>
     </div>
@@ -43,7 +43,7 @@
   .tags {
     background: white;
 
-    > li {
+    > .tag {
       display: flex;
       align-items: center;
       font-size: 16px;
