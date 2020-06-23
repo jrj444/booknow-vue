e8e86cb8e45c2ed65f1c34e0a244014bd50f7c70
jiango <jrj444@foxmail.com>
2020-06-23 14:57:47 +0800

    封装模块化store，删除了models

--- src/store/recordStore.ts
+++ src/store/recordStore.ts
@@ -1,6 +1,24 @@
-import {recordListModel} from '@/models/recordListModel';
+import clone from '@/lib/clone';
 
-export default {
-  recordList: recordListModel.fetch(),
-  createRecord: (record: RecordItem) => recordListModel.createItem(record),
-}
+const localStorageKeyName = 'recordList';
+
+const recordStore = {
+  recordList: [] as RecordItem[],
+  fetchRecords() {
+    this.recordList = JSON.parse(window.localStorage.getItem(localStorageKeyName) || '[]') as RecordItem[];
+    return this.recordList;
+  },
+  saveRecords() {
+    window.localStorage.setItem(localStorageKeyName, JSON.stringify(this.recordList));
+  },
+  createRecord(record: RecordItem) {
+    const recordClone: RecordItem = clone(record);
+    recordClone.createdAt = new Date();
+    this.recordList && this.recordList.push(recordClone);
+    recordStore.saveRecords();
+  }
+};
+
+recordStore.fetchRecords();
+
+export default recordStore;
--- src/store/tagStore.ts
+++ src/store/tagStore.ts
@@ -1,22 +1,60 @@
-import {tagListModel} from '@/models/tagListModel';
+import createId from '@/lib/createId';
 
-export default {
-  tagList: tagListModel.fetch(),
-  findTag (id: string) {
-    return this.tagList.filter(tag => tag.id === id)[0];
+const localStorageKeyName = 'tagList';
+
+const tagStore = {
+  tagList: [] as Tag[],
+  fetchTags() {
+    this.tagList = JSON.parse(window.localStorage.getItem(localStorageKeyName) || '[]');
+    return this.tagList;
   },
-  createTag: (name: string) => {
-    const message = tagListModel.create(name);
-    if (message === 'duplicated') {
+  findTag(id: string) {
+    return this.tagList.filter(t => t.id === id);
+  },
+  createTag(name: string) {
+    const names = this.tagList.map(item => item.name);
+    if (names.indexOf(name) >= 0) {
       window.alert('标签名重复啦，请重新创建！');
-    } else if (message === 'success') {
-      window.alert('添加成功！');
+      return 'duplicated';
+    }
+    const id = createId().toString();
+    this.tagList.push({id: id, name: name});
+    this.saveTags();
+    return 'success';
+  },
+  removeTag(id: string) {
+    let index = -1;
+    for (let i = 0; i < this.tagList.length; i++) {
+      if (this.tagList[i].id === id) {
+        index = i;
+        break;
+      }
+    }
+    this.tagList.splice(index, 1);
+    this.saveTags();
+    return true;
+  },
+  updateTag(id: string, name: string) {
+    const idList = this.tagList.map(item => item.id);
+    if (idList.indexOf(id) >= 0) {
+      const names = this.tagList.map(item => item.id);
+      if (names.indexOf(name) >= 0) {
+        return 'duplicated';
+      } else {
+        const tag = this.tagList.filter(item => item.id === id)[0];
+        tag.name = name;
+        this.saveTags();
+        return 'success';
+      }
+    } else {
+      return 'not_found';
     }
   },
-  removeTag: (id: string) => {
-    return tagListModel.remove(id);
-  },
-  updateTag: (id: string, name: string) => {
-    return tagListModel.update(id, name);
+  saveTags(){
+    window.localStorage.setItem(localStorageKeyName, JSON.stringify(this.tagList));
   }
-}
+};
+
+tagStore.fetchTags();
+
+export default tagStore;
