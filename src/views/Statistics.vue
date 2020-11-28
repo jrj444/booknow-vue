e79319c76727d8efdc9d2e8c4e9ffb37b949227d
jrj444 <jrj444@foxmail.com>
2020-11-28 18:13:47 +0800

    配置 Chart 样式

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,6 +1,8 @@
 <template>
   <Layout>
-    <Chart :options="x"></Chart>
+    <div class="chart-wrapper">
+      <Chart class="chart" :options="x"></Chart>
+    </div>
   </Layout>
 </template>
 
@@ -15,23 +17,42 @@ import Chart from '@/components/Chart.vue';
 export default class Statistics extends Vue {
   get x() {
     return {
+      grid: {
+        left: 0,
+        right: 0
+      },
       xAxis: {
         type: 'category',
-        data: ['1', '2', '3', '4', '5', '6', '7']
+        data: ['1', '2', '3', '4', '5', '6', '7'],
+        axisTick: {alignWithLabel: true},
+        axisLine: {lineStyle: {color: '#777'}}
       },
       yAxis: {
-        type: 'value'
+        type: 'value',
+        show: false
       },
       series: [{
+        symbolSize: 10,
         data: [100, 200, 300, 200, 400, 500, 140],
         type: 'line'
       }],
-      tooltip: {show: true}
+      tooltip: {
+        show: true,
+        triggerOn: 'click',
+        formatter: '{c}',
+        position: 'top'
+      }
     };
   }
 }
 </script>
 
 <style scoped lang="scss">
+.chart-wrapper {
+  overflow: auto;
 
+  > .chart {
+    width: 430%;
+  }
+}
 </style>
