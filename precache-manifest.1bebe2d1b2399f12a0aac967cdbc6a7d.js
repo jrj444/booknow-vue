239391b4710550ba0d0546a153c5400cf51a41d0
jrj444 <jrj444@foxmail.com>
2020-12-09 22:44:25 +0800

    deploy

---
+++ precache-manifest.1bebe2d1b2399f12a0aac967cdbc6a7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d042f7b3aebee9f53cd5",
+    "url": "/booknow-vue/css/app.a0921e8f.css"
+  },
+  {
+    "revision": "91434b713a95157e7aac6cd6a4d7ea9e",
+    "url": "/booknow-vue/index.html"
+  },
+  {
+    "revision": "d042f7b3aebee9f53cd5",
+    "url": "/booknow-vue/js/app.27cc3b27.js"
+  },
+  {
+    "revision": "b84d522cdc95946b65ae",
+    "url": "/booknow-vue/js/chunk-vendors.86894c7d.js"
+  },
+  {
+    "revision": "31604ec356a4be236c625255b2dfa088",
+    "url": "/booknow-vue/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/booknow-vue/robots.txt"
+  }
+]);
